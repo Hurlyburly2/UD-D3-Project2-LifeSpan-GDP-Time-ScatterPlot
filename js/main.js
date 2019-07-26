420b7f9a6a62a3a8cc19d8f0125d59b0049dfb90
Hurlyburly2 <dougdougmann@gmail.com>
2019-07-26 02:33:48 -0400

    canvas setup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+let margin = { top: 10, right: 10, bottom: 200, left: 100 }
+
+let canvasWidth = 700 - margin.right - margin.left
+let canvasHeight = 550 - margin.top - margin.bottom
+
+let svg = d3.select('#chart-area').append('svg')
+	.attr('width', canvasWidth + margin.right + margin.left)
+	.attr('height', canvasHeight + margin.top + margin.bottom)
+	
+let graphGroup = svg.append('g')
+	.attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+
 d3.json("data/data.json").then(data => {
 	
 	data.forEach(year => {
@@ -15,6 +27,24 @@ d3.json("data/data.json").then(data => {
 		year.year = parseInt(year)
 	})
 	
+	d3.interval(() => {
+		update(data)
+	}, 500)
+	update(data)
+	
 }).catch(error => {
 	console.log(error)
 })
+
+const update = (data) => {
+	let points = graphGroup.selectAll('rect')
+		.data(data, (data) => { return data.year })
+	
+	points.enter()
+		.append('rect')
+		.attr('x', 0)
+		.attr('y', 0)
+		.attr('width', canvasWidth)
+		.attr('height', canvasHeight)
+		.attr('fill', 'grey')
+}
